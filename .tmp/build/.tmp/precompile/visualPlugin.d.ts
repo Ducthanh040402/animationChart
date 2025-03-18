f4adf3222bb2c2bce1702e565452aa461af9c3e7
Ducthanh040402 <thanh442002@gmail.com>
2025-03-18 22:43:28 +0700

    Ignore.tmp folder

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var PlayChart_DEBUG: IVisualPlugin;
-export default PlayChart_DEBUG;
